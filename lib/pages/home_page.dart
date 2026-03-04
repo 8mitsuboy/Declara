8c5eadff9f520dce7548131bb42c97830bb8cb66
8mitsuboy <hachimitsu8897@icloud.com>
2026-03-04 23:07:04 +0900

    画面を分割

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:declara/pages/create_todo_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -9,52 +10,11 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = useTextEditingController();
-    final colorScheme = Theme.of(context).colorScheme;
     final currentPageIndex = useState<int>(0);
 
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: [
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextField(
-                controller: controller,
-                decoration: InputDecoration(
-                  labelText: "やりたいこと",
-                  hintText: "例: 引っ越し業者を比較する",
-                  prefixIcon: const Icon(Icons.edit_note),
-                  suffixIcon: IconButton(
-                    onPressed: controller.clear,
-                    icon: const Icon(Icons.close),
-                  ),
-                  filled: true,
-                  fillColor: colorScheme.surfaceContainerHighest.withValues(
-                    alpha: 0.4,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide(
-                      color: colorScheme.primary,
-                      width: 2,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(onPressed: () => {}, child: const Text("やらなきゃ")),
-            ],
-          ),
-        ),
-        const TodoPage(),
-      ][currentPageIndex.value],
+      body: [const CreateTodoPage(), const TodoPage()][currentPageIndex.value],
       bottomNavigationBar: NavigationBar(
         selectedIndex: currentPageIndex.value,
         onDestinationSelected: (int index) {
