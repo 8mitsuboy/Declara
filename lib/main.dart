a8e52ead1257583c686bb915d42c6b80a0e8eb06
8mitsuboy <hachimitsu8897@icloud.com>
2026-03-07 21:43:02 +0900

    ➕ flutter_dotenvを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:declara/pages/home_page/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(ProviderScope(child: MyApp()));
 }
 
