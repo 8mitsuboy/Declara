1200170e3baba66c9cec703acd60406125363155
8mitsuboy <hachimitsu8897@icloud.com>
2026-03-07 21:11:55 +0900

    🎨 ディレクトリ構成修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:declara/pages/home_page.dart';
+import 'package:declara/pages/home_page/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
