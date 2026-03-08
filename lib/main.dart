474b37ef36003d793ecf54da0f2c80546ffa7ff0
8mitsuboy <hachimitsu8897@icloud.com>
2026-03-08 09:19:03 +0900

    💄 AppBarのスタイル修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:declara/pages/home_page/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
@@ -11,12 +12,28 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final colorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
+    final textTheme = GoogleFonts.mPlusRounded1cTextTheme();
+
     return MaterialApp(
       title: 'Declara',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: colorScheme,
+        textTheme: textTheme,
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          backgroundColor: colorScheme.surface,
+          foregroundColor: colorScheme.onSurface,
+          elevation: 0,
+          titleTextStyle: GoogleFonts.mPlusRounded1c(
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+            color: colorScheme.primary,
+          ),
+        ),
+      ),
       home: const HomePage(title: 'Declara'),
     );
   }
